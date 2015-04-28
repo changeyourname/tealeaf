8d96c924526de5c225507cfee77ffb3129b4bbef
David Clawson <david.clawson@gmail.com>
2015-04-27 22:31:45 -0600

    Add flow control and loops and iterators. Enough for the night.

---
+++ flow_control/flow_exercises.rb
@@ -0,0 +1,34 @@
+# flow_exercises.rb
+
+
+state = "GO"
+
+while state == "GO"
+	puts "Type something in you want made all caps, type 'STOP' to stop"
+	user_input = gets.chomp
+	if user_input == "STOP"
+		state = "STOP"
+	end
+	puts user_input.upcase
+end
+
+some_array = [1,3,5,7,11,13,17]
+
+some_array.each_with_index {|number, index|
+	puts "The number is #{number}, and the index is #{index}"
+}
+
+
+puts "I'm going to countdown to zero from a number you give me, okay?"
+user_number = gets.chomp.to_i
+
+def recursive_countdown(number)
+	if number < 0
+		return
+	else
+		puts number
+		recursive_countdown (number - 1)
+	end
+end
+
+recursive_countdown(user_number)
---
+++ flow_control/loop_example.rb
@@ -0,0 +1,5 @@
+# loop_example.rb |
+
+loop do
+	puts "This will keep repeating. Make it stop!"
+end
